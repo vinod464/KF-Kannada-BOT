1c5170120b03c46e93160c80860d5af9504138cc
ujwalnayak3 <115396876+ujwalnayak3@users.noreply.github.com>
2025-11-07 14:01:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ auth_channel = environ.get('AUTH_CHANNEL', '-1003194562013') # give your force s
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
-reqst_channel = environ.get('REQST_CHANNEL', '-1001722189873')
+reqst_channel = environ.get('REQST_CHANNEL', '-1003205136575')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Channel Is For Index Request 
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/kannada_cineflix_request')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Kannada_movies_request_only')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Kannada_Cineflix')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Kannada_Cineflix') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/proud_kannadiga')
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
